61a2a8ba44817b563cbb300dee60572b809f42ab
Esther N <estheroids@gmail.com>
2020-08-02 01:26:34 -0400

    mongoose connect server.js

--- server.js
+++ server.js
@@ -9,15 +9,30 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/indegodb");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/indegodb");
+
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/axesearch",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4, // Use IPv4, skip trying IPv6
+  },
+  () => console.log("Connected to the the DB!")
+);
 
 // Start the API server
 app.listen(PORT, function () {
-    console.log(`🌎  ==> API Server now listening on PORT https://localhost/${PORT}!`);
+  console.log(
+    `🌎  ==> API Server now listening on PORT https://localhost/${PORT}!`
+  );
 });
